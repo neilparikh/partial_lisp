7b7e6c14385749270bc31c226250b8a3905235d0
Neil Parikh <parikh.neil@me.com>
2015-04-15 22:02:07 -0400

    change the example

--- example.lisp
+++ example.lisp
@@ -1,2 +1,7 @@
 (def-fun (power x n) (if (= n 0) 1 (* x (power x (- n 1)))))
-(print (power (get-int) 3))
+(define z (+ 1 2))
+(print (power x 3))
+(print (power y 5))
+(print (power z 2))
+(if (> 3 2) (+ 1234 2342) 5)
+(if (< 3 2) (* 100 2) 5)
